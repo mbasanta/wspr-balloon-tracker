f46d2aef71b2ada5a746dff20b4a08ca41378971
Matthew Basanta <matthew.basanta@hdrinc.com>
2024-07-23 13:40:27 -0400

    Update MaidenheadService.ts

--- src/services/MaidenheadService.ts
+++ src/services/MaidenheadService.ts
@@ -1,8 +1,8 @@
 import { LatLong } from "../types/LatLong";
 
-export function gridSquareToCoordinates(gridSquare: string): LatLong {
-  const charA = "A".charCodeAt(0);
+const CHAR_A = "A".charCodeAt(0);
 
+export function gridSquareToCoordinates(gridSquare: string): LatLong {
   if (gridSquare.length !== 4 && gridSquare.length !== 6) {
     throw new Error("Invalid grid square");
   }
@@ -11,20 +11,20 @@ export function gridSquareToCoordinates(gridSquare: string): LatLong {
   let lon = -180;
   let lat = -90;
 
-  let baseLat = (gridSquareUpper.charCodeAt(1) - charA) * 10;
+  let baseLat = (gridSquareUpper.charCodeAt(1) - CHAR_A) * 10;
   let detailLat = parseInt(gridSquareUpper[3]);
   lat += baseLat + detailLat;
 
   if (gridSquareUpper.length === 6) {
-    lat += (gridSquareUpper.charCodeAt(5) - charA) / 24 + 1 / 48;
+    lat += (gridSquareUpper.charCodeAt(5) - CHAR_A) / 24 + 1 / 48;
   }
 
-  let baseLon = (gridSquareUpper.charCodeAt(0) - charA) * 20;
+  let baseLon = (gridSquareUpper.charCodeAt(0) - CHAR_A) * 20;
   let detailLon = parseInt(gridSquareUpper[2]) * 2;
   lon += baseLon + detailLon;
 
   if (gridSquareUpper.length === 6) {
-    lon += (gridSquareUpper.charCodeAt(4) - charA) / 12 + 1 / 24;
+    lon += (gridSquareUpper.charCodeAt(4) - CHAR_A) / 12 + 1 / 24;
   }
 
   return { Lat: lat, Long: lon };
