8aa638b67129342b2839bc7ea95ad91ab94df08e
Matthew Basanta <matthew.basanta@hdrinc.com>
2024-07-26 15:16:10 -0400

    Display all points on map

--- src/components/WsprLocationsMap/WsprLocationsMap.tsx
+++ src/components/WsprLocationsMap/WsprLocationsMap.tsx
@@ -1,36 +1,57 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import WsprLocation from "../../classes/WsprLocation";
-import useWsprLocationsMap from "./useWsprLocationsMap";
 
 import { Icon } from "leaflet";
 import "leaflet/dist/leaflet.css";
+import { useEffect, useState } from "react";
 import classes from "./WsprLocationsMap.module.css";
 
 export default function WsprLocationsMap(props: { wsprData: WsprLocation[] }) {
-  const { wsprData } = props;
-  const { position } = useWsprLocationsMap({ wsprData });
+  const [position, setPosition] = useState({ lat: 38, lng: -84.5 });
+  const [pointIcons, setPointIcons] = useState<JSX.Element[]>([]);
 
   const icon = new Icon({
     iconUrl: "./map-dot.svg",
     iconSize: [35, 35],
   });
 
+  useEffect(() => {
+    if (props.wsprData.length > 0) {
+      let pointIcons = props.wsprData
+        .filter((wsprData) => wsprData.gpsValid)
+        .map((wsprData) => {
+          return (
+            <Marker
+              icon={icon}
+              position={{ lat: wsprData.lat, lng: wsprData.long }}
+            >
+              <Popup>
+                <div>
+                  <p>{wsprData.callsign}</p>
+                  <p>{wsprData.locator}</p>
+                  <p>{wsprData.dBm} dBm</p>
+                  <p>{wsprData.altitude} m</p>
+                </div>
+              </Popup>
+            </Marker>
+          );
+        });
+      setPointIcons(pointIcons);
+    }
+  }, [props.wsprData]);
+
   return (
     <MapContainer
       className={classes.wsprLocationsMap}
       center={position}
-      zoom={13}
+      zoom={9}
       scrollWheelZoom={true}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker icon={icon} position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {pointIcons}
     </MapContainer>
   );
 }
